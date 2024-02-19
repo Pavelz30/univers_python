ca80e8ceb86fcc4181f4b647d43aaae0f16f61ee
Pavel <pavel.zavodyan@mail.ru>
2024-02-19 16:44:27 +0300

    add first option of task4

---
+++ Tsap/task4.py
@@ -0,0 +1,9 @@
+# Option 1 (1)
+def func1(n):
+    if n == 0:
+        return 0.72
+    if n == 1:
+        return -0.93
+    if n >= 2:
+        return func1(n-2) ** 2 + func1(n - 2) + \
+            func1(n - 1) ** 3 - func1(n-1) ** 3
