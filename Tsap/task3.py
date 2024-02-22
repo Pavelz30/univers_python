88300584ea1768bdf2ad29670f7990c9d2703247
Pavel <pavel.zavodyan@mail.ru>
2024-02-22 20:17:45 +0300

    Completed 2 and 3 tasks

--- Tsap/task3.py
+++ Tsap/task3.py
@@ -1,5 +1,7 @@
 import math
 
+# Option 1
+
 
 def func1(n, m, b):
     res = 0
@@ -8,6 +10,8 @@ def func1(n, m, b):
             for c in range(1, n + 1):
                 res += (65 * math.tan(c) + math.pow(math.sin(i), 7) + k ** 4)
     return res
+
+# Option 2
 
 
 def func2(n, m, b):
@@ -25,12 +29,16 @@ def func2(n, m, b):
         k += 1
     return res
 
+# Option 3
+
 
 def func3(n, m, b):
     matrix = [(65 * math.tan(c) + math.pow(math.sin(i), 7) + k ** 4)
               for c in range(1, n + 1) for i in range(1, m + 1)
               for k in range(1, b + 1)]
     return sum(matrix)
+
+# Option 4
 
 
 def func4(n, m, b):
@@ -39,3 +47,26 @@ def func4(n, m, b):
               for k in range(1, b + 1))
     return sum(matrix)
 
+# Option 5
+
+
+def f2(n, m, b):
+    if n == 0:
+        return 0
+    return 65 * math.tan(n) + math.pow(math.sin(m), 7) \
+        + b ** 4 + f2(n - 1, m, b)
+
+
+def f1(n, m, b):
+    if m == 0:
+        return 0
+    return f2(n, m, b) + f1(n, m - 1, b)
+
+
+def main(n, m, b):
+    if b == 0:
+        return 0
+    return f1(n, m, b) + main(n, m, b - 1)
+
+
+print(main(7, 3, 2))
--- Tsap/task4.py
+++ Tsap/task4.py
@@ -13,13 +13,32 @@ def func1(n):
 def func2(n):
     return 0.72 if n == 0 else -0.93 if \
         n == 1 else func1(n - 2) ** 2 + func1(n - 2) + \
-        func1(n - 1) ** 3 - func1(n - 1) ** 3 if n >= 2 else 0
+                    func1(n - 1) ** 3 - func1(n - 1) ** 3 if n >= 2 else 0
 
 
 # Option 3 (3)
 def func3(n):
     arr = [0.72, -0.93]
-    for i in range(2, n+1):
-        arr.append(arr[i-2] ** 2 + arr[i - 2] + arr[i - 1] ** 3 - arr[i - 1] ** 3)
+    for i in range(2, n + 1):
+        arr.append(arr[i - 2] ** 2 + arr[i - 2] + arr[i - 1] ** 3 - arr[i - 1] ** 3)
     return arr[-1]
+
+
+# Option 4 (2)
+def func4(n):
+    a, b = 0.72, -0.93
+    for _ in range(2, n + 1):
+        a, b = b, a ** 2 + a + b ** 3 - b ** 3
+    return b
+
+# Option 5 (4)
+def main(n):
+    match n:
+        case 0:
+            return 0.72
+        case 1:
+            return -0.93
+        case _:
+            return func1(n - 2) ** 2 + func1(n - 2)
+
 
